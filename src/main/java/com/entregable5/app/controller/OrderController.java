b8a36d83c8b90e51e58b3095de96c4d7a7aed372
seba <sebaesains77@gmail.com>
2022-06-29 19:58:28 -0300

    order controller

--- src/main/java/com/entregable5/app/controller/OrderController.java
+++ src/main/java/com/entregable5/app/controller/OrderController.java
@@ -1,46 +1,86 @@
 package com.entregable5.app.controller;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.entregable5.app.model.Order;
+import com.entregable5.app.service.OrderService;
 
 
 @RestController
-@RequestMapping("/api/products")
+@RequestMapping("/api/orders")
 public class OrderController implements Controller<Order> {
 
+	@Autowired
+	private OrderService orderService;
+	
+	// create a new order
+	//@RequestMapping(method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
 	@Override
-	public ResponseEntity<?> create(Order t) {
-		// TODO Auto-generated method stub
-		return null;
+	@PostMapping
+	public ResponseEntity<?> create(@RequestBody Order o) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(orderService.save(o));
 	}
-
+	
+	// List of orders
 	@Override
-	public List<Order> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+	@GetMapping
+	public List<Order> findAll(){
+		return orderService.findAll();
 	}
-
+	
+	// read an order
 	@Override
-	public ResponseEntity<?> getById(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+	@GetMapping("/{id}")
+	public ResponseEntity<?> getById(@PathVariable Long id) {
+		Optional<Order> oOrder = orderService.findById(id);
+		
+		if(!oOrder.isPresent()) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.ok(oOrder);
 	}
-
+	
+	// update an order
 	@Override
-	public ResponseEntity<?> update(Order t, Long id) {
-		// TODO Auto-generated method stub
-		return null;
+	@PutMapping("/{id}")
+	public ResponseEntity<?> update(@RequestBody Order o, @PathVariable Long id) {
+		Optional<Order> oOrder = orderService.findById(id);
+		
+		if(!oOrder.isPresent()) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		oOrder.get().setFechaCompra(o.getFechaCompra());
+		oOrder.get().setCliente(o.getCliente());
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(orderService.save(oOrder.get()));
 	}
-
+	
+	// delete an client
 	@Override
-	public ResponseEntity<?> delete(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> delete(@PathVariable Long id) {
+		Optional<Order> oOrder = orderService.findById(id);
+		
+		if(!oOrder.isPresent()) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		orderService.deleteById(id);
+		return ResponseEntity.ok().build();
 	}
 
 }
+
--- src/main/java/com/entregable5/app/service/OrderService.java
+++ src/main/java/com/entregable5/app/service/OrderService.java
@@ -3,7 +3,6 @@ package com.entregable5.app.service;
 import java.util.List;
 import java.util.Optional;
 
-import com.entregable5.app.model.Client;
 import com.entregable5.app.model.Order;
 
 public interface OrderService {
