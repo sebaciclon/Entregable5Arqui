e260d5982b47e348be20e3ee8cf2edb8e5e3f93f
seba <sebaesains77@gmail.com>
2022-07-05 09:34:24 -0300

    se emprolijo el codigo

--- src/main/java/com/entregable5/app/repository/OrderDetailRepository.java
+++ src/main/java/com/entregable5/app/repository/OrderDetailRepository.java
@@ -1,9 +1,11 @@
 package com.entregable5.app.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.entregable5.app.model.OrderDetail;
 
+@Repository
 public interface OrderDetailRepository extends JpaRepository<OrderDetail, Long>{
 	
 }
